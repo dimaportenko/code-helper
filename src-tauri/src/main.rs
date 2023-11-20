34fc6a2ff26b1ad78ca0d85a2495700925a0076d
Dima Portenko <troublesohard@gmail.com>
2023-11-20 21:17:35 +0200

    setup global shortcuts

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, LogicalSize, Size};
+use tauri::{
+    CustomMenuItem, GlobalShortcutManager, Manager, Size, SystemTray, SystemTrayEvent,
+    SystemTrayMenu,
+};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,66 +12,67 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+const SHORTCUT_CAPTURE: &str = "Command+Option+l";
+
 fn system_tray() -> SystemTray {
     SystemTray::new().with_menu(
         SystemTrayMenu::new()
             .add_item(
-                CustomMenuItem::new("capture".to_string(), "Capture")
-                    .accelerator("Command+Option+l"),
+                CustomMenuItem::new("capture".to_string(), "Capture").accelerator(SHORTCUT_CAPTURE),
             )
             .add_item(CustomMenuItem::new("exit".to_string(), "Quit").accelerator("Q")),
     )
+}
+
+fn toggle_overlay_window(app: &tauri::AppHandle) {
+    let overlay = app.get_window("overlay");
+    // if overlay exist
+    if let Some(overlay) = overlay {
+        // if overlay is visible
+        if overlay.is_visible().unwrap() {
+            // hide it
+            overlay.hide().unwrap();
+        } else {
+            // show it
+            overlay.show().unwrap();
+        }
+    } else {
+        // if overlay not exist create new
+        let overlay =
+            tauri::WindowBuilder::new(app, "overlay", tauri::WindowUrl::App("overlay.html".into()))
+                .title("Overlay")
+                // .maximized(true)
+                // .minimizable(true)
+                .resizable(false)
+                .transparent(true)
+                .decorations(false)
+                .position(0.0, 0.0)
+                // .fullscreen(true)
+                .build()
+                .unwrap();
+
+        let monitor = match overlay.current_monitor() {
+            Ok(mon) => mon,
+            Err(_) => panic!("No monitor found!"),
+        }
+        .unwrap();
+
+        let physical_size = monitor.size().clone();
+        let size = Size::from(physical_size);
+        // let size = LogicalSize
+        let _ = overlay.set_size(size);
+
+        // overlay.set_size(overlay.current_monitor().size());
+        // show it
+        overlay.show().unwrap();
+    }
 }
 
 fn on_system_tray_event<'a>(app: &'a tauri::AppHandle, event: SystemTrayEvent) {
     match event {
         SystemTrayEvent::MenuItemClick { id, .. } => {
             match id.as_str() {
-                "capture" => {
-                    let overlay = app.get_window("overlay");
-                    // if overlay exist
-                    if let Some(overlay) = overlay {
-                        // if overlay is visible
-                        if overlay.is_visible().unwrap() {
-                            // hide it
-                            overlay.hide().unwrap();
-                        } else {
-                            // show it
-                            overlay.show().unwrap();
-                        }
-                    } else {
-                        // if overlay not exist create new
-                        let overlay = tauri::WindowBuilder::new(
-                            app,
-                            "overlay",
-                            tauri::WindowUrl::App("overlay.html".into()),
-                        )
-                        .title("Overlay")
-                        // .maximized(true)
-                        // .minimizable(true)
-                        .resizable(false)
-                        .transparent(true)
-                        .decorations(false)
-                        .position(0.0, 0.0)
-                        // .fullscreen(true)
-                        .build()
-                        .unwrap();
-
-                        let monitor = match overlay.current_monitor() {
-                            Ok(mon) => mon,
-                            Err(_) => panic!("No monitor found!"),
-                        }.unwrap();
-
-                        let physical_size = monitor.size();
-                        let size = Size::from(*physical_size);
-                        // let size = LogicalSize
-                        let _ = overlay.set_size(size);
-
-                        // overlay.set_size(overlay.current_monitor().size());
-                        // show it
-                        overlay.show().unwrap();
-                    }
-                }
+                "capture" => toggle_overlay_window(app),
                 "exit" => {
                     // Quit the app
                     std::process::exit(0);
@@ -80,8 +84,25 @@ fn on_system_tray_event<'a>(app: &'a tauri::AppHandle, event: SystemTrayEvent) {
     }
 }
 
+fn setup_shortcuts(app: &tauri::App) {
+    let app_handle = app.app_handle();
+    let mut manager = app.global_shortcut_manager();
+
+    // Clone app_handle for the closure
+    let app_handle_clone = app_handle.clone();
+    manager
+        .register(SHORTCUT_CAPTURE, move || {
+            toggle_overlay_window(&app_handle_clone);
+        })
+        .expect("Failed to register global shortcut");
+}
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            setup_shortcuts(app);
+            Ok(())
+        })
         .system_tray(system_tray())
         .on_system_tray_event(on_system_tray_event)
         .invoke_handler(tauri::generate_handler![greet])
