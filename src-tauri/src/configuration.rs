b5291fd91426ecdca4c016eb154c356d0b74027d
Dima Portenko <troublesohard@gmail.com>
2023-11-21 10:23:29 +0200

    refactor: move functions outside main.rs

---
+++ src-tauri/src/configuration.rs
@@ -0,0 +1,45 @@
+use tauri::{
+    CustomMenuItem, GlobalShortcutManager, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
+};
+
+use crate::overlay::toggle_overlay_window;
+const SHORTCUT_CAPTURE: &str = "Command+Ctrl+a";
+
+pub fn system_tray() -> SystemTray {
+    SystemTray::new().with_menu(
+        SystemTrayMenu::new()
+            .add_item(
+                CustomMenuItem::new("capture".to_string(), "Capture").accelerator(SHORTCUT_CAPTURE),
+            )
+            .add_item(CustomMenuItem::new("exit".to_string(), "Quit").accelerator("Q")),
+    )
+}
+
+pub fn on_system_tray_event<'a>(app: &'a tauri::AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { id, .. } => {
+            match id.as_str() {
+                "capture" => toggle_overlay_window(app),
+                "exit" => {
+                    // Quit the app
+                    std::process::exit(0);
+                }
+                _ => {}
+            }
+        }
+        _ => {}
+    }
+}
+
+pub fn setup_shortcuts(app: &tauri::App) {
+    let app_handle = app.app_handle();
+    let mut manager = app.global_shortcut_manager();
+
+    // Clone app_handle for the closure
+    let app_handle_clone = app_handle.clone();
+    manager
+        .register(SHORTCUT_CAPTURE, move || {
+            toggle_overlay_window(&app_handle_clone);
+        })
+        .expect("Failed to register global shortcut");
+}
---
+++ src-tauri/src/overlay.rs
@@ -0,0 +1,41 @@
+use tauri::{Size, Manager};
+
+pub fn toggle_overlay_window(app: &tauri::AppHandle) {
+    let overlay = app.get_window("overlay");
+    // if overlay exist
+    if let Some(overlay) = overlay {
+        // if overlay is visible
+        if overlay.is_visible().unwrap() {
+            // hide it
+            overlay.hide().unwrap();
+        } else {
+            // show it
+            overlay.show().unwrap();
+        }
+    } else {
+        // if overlay not exist create new
+        let overlay =
+            tauri::WindowBuilder::new(app, "overlay", tauri::WindowUrl::App("overlay.html".into()))
+                .resizable(false)
+                .transparent(true)
+                .decorations(false)
+                .position(0.0, 0.0)
+                .build()
+                .unwrap();
+
+        let monitor = match overlay.current_monitor() {
+            Ok(mon) => mon,
+            Err(_) => panic!("No monitor found!"),
+        }
+        .unwrap();
+
+        let physical_size = monitor.size().clone();
+        let size = Size::from(physical_size);
+        // let size = LogicalSize
+        let _ = overlay.set_size(size);
+
+        // overlay.set_size(overlay.current_monitor().size());
+        // show it
+        overlay.show().unwrap();
+    }
+}
